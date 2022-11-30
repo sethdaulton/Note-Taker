8b970fb3b72210c4208081eb841c67976b81bf7a
Seth Daulton <seth.daulton@gmail.com>
2022-11-30 14:42:55 -0600

    Added updates to the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Note-Taker
+
+## Description 
+
+For this challenge assignment I was tasked with creating a functional note taking web application. When I open the note web application I am presented with a landing page. When I click the "Get Started" button I am presented with a page that has two columns (one for previously saved notes and one for a note that is actively being created). When I complete the note title and text fields I am presented with a button to save the note. Then the note appears on the left hand column below the previously created notes. I can also delete a note by clicking the trash can icon on a note that has been previously created. 
+
+## Screenshot
+
+
+## Link to deployed application
--- routes/notes.js
+++ routes/notes.js
@@ -8,26 +8,27 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
-// tips.post('/', (req, res) => {
-//   console.info(`${req.method} request received to add a tip`);
-//   console.log(req.body);
+// POST Route for a new UX/UI notes
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+});
 
-//   const { username, topic, tip } = req.body;
+  // const { username, topic, note } = req.body;
 
-//   if (req.body) {
-//     const newTip = {
-//       username,
-//       tip,
-//       topic,
-//       tip_id: uuid(),
-//     };
+  // if (req.body) {
+  //   const newNote = {
+  //     username,
+  //     tip,
+  //     topic,
+  //     tip_id: uuid(),
+  //   };
 
-//     readAndAppend(newTip, './db/tips.json');
-//     res.json(`Tip added successfully 🚀`);
-//   } else {
-//     res.error('Error in adding tip');
-//   }
-// });
+  //   readAndAppend(newNote, './db/notes.json');
+  //   res.json(`Tip added successfully 🚀`);
+  // } else {
+  //   res.error('Error in adding tip');
+  // }
+
 
 module.exports = notes;
