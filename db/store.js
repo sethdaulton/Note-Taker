940b1b3d86ffb554f4b5b89abbf58432ffde615f
Seth Daulton <seth.daulton@gmail.com>
2022-11-30 18:52:50 -0600

    wrote new code, deleted unnecessary files, created new files

---
+++ db/store.js
@@ -0,0 +1,45 @@
+const util = require('util');
+const fs = require('fs');
+
+const uuid = require('uuid/v1');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read(){
+        return readFileAsync('db/db.json', 'utf8');
+    }
+    write(note){
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+    getNotes(){
+        return this.read().then((notes)=>{
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes))
+            } catch (error) {
+               parsedNotes = [] 
+            }
+            return parsedNotes
+        })
+    }
+    addNote(note){
+        const { title, text } = note
+        if(!title || !text) {
+            throw new Error('title and text cannot be blank')
+        }
+        const newNote = { title, text, id:uuid()}
+        return this.getNotes()
+        .then((notes) => [...notes,newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote)
+    }
+    removeNote(id){
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id ))
+        .then((filteredNotes) => this.write(filteredNotes))
+    }
+}
+
+module.exports = new Store();
